43fc252536a1eedc9269d9759323d39a68706ee8
Maxim Litvinov <maximlitvinov.2611@gmail.com>
2023-01-24 13:32:31 +0300

    Add React routing

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './Home.module.scss';
+
+type Props = {};
+
+const Home = (props: Props) => {
+  return <div className={style.home}>Home</div>;
+};
+
+export default Home;
---
+++ src/pages/MonthStatistics/MonthStatistics.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './MonthStatistics.module.scss';
+
+type Props = {};
+
+const MonthStatistics = (props: Props) => {
+  return <div className={style.month}>Month Statistics</div>;
+};
+
+export default MonthStatistics;
