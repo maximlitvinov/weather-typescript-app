f4929d735b799d6ccf305f5c5c5f92dbac2f08a7
Maxim Litvinov <maximlitvinov.2611@gmail.com>
2023-02-06 11:03:13 +0300

    Work with Theme in Local Storage

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,15 +1,19 @@
 import { ReactNode, useState } from 'react';
+
 import { Theme, ThemeContext } from '../context/ThemeContext';
 import { changeRootVariables } from '../model/ChangeRootVariables';
+import { storage } from '../model/Storage';
 
 interface Props {
   children: ReactNode;
 }
 
 export const ThemeProvider = ({ children, ...props }: Props) => {
-  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+  const [theme, setTheme] = useState<Theme>(storage.getItem('theme') || Theme.LIGHT);
+  changeRootVariables(theme);
 
   function changeTheme(theme: Theme) {
+    storage.setItem('theme', theme);
     setTheme(theme);
     changeRootVariables(theme);
   }
